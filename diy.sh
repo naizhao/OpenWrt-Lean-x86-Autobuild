bfb8abefab5ae0947d6bcb532ef1dd6608fd2f78
A 38D Bra <hongsheng@gmail.com>
2020-03-05 18:36:11 +0800

    enable ssr+

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/exit 0/echo 0xDEADBEEF > \/etc\/config\/google_fu_mode\nexit 0/g' package/lean/default-settings/files/zzz-default-settings
